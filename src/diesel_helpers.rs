8716a9d6374754a48205ffaa7764b438ac4240a9
RomainC <bebert64@gmail.com>
2023-11-01 12:48:32 +0100

    create connection_string from various env variables

--- src/diesel_helpers.rs
+++ src/diesel_helpers.rs
@@ -2,12 +2,28 @@ use crate::DonResult;
 
 use {
     diesel::{pg::PgConnection, prelude::*},
-    dotenv::dotenv,
     std::env,
 };
 
-pub(crate) fn db() -> DonResult<PgConnection> {
-    dotenv()?;
-    let db_url = env::var("DATABASE_URL")?;
+pub fn db() -> DonResult<PgConnection> {
+    let mode = match env::var("MODE").ok().as_deref() {
+        Some("PROD") => Mode::Prod,
+        _ => Mode::Dev,
+    };
+    let user = env::var("RW_USERNAME")?;
+    let password = env::var("RW_PASSWD")?;
+    let ip = env::var("DB_IPV6")?;
+    let port = env::var("DB_PORT")?;
+    let db_name = match mode {
+        Mode::Dev => env::var("DB_NAME_DEV")?,
+        Mode::Prod => env::var("DB_NAME_PROD")?,
+    };
+    let db_url = format!("postgres://{user}:{password}@[{ip}]:{port}/{db_name}",);
     Ok(PgConnection::establish(&db_url)?)
+}
+
+#[derive(Debug)]
+enum Mode {
+    Dev,
+    Prod,
 }
