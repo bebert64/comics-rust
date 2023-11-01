a52f21bd4ba3ebee3767474b8533098ffab9545c
RomainC <bebert64@gmail.com>
2023-11-01 12:46:36 +0100

    remove dotenv dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
 #![allow(non_snake_case)]
 
-pub mod actix;
 pub mod data_recovery;
 mod diesel_helpers;
+pub mod rest;
 mod schema;
 
 use {
     don_error::DonResult,
-    dotenv::dotenv,
     std::{env::var, path::PathBuf},
 };
+
+pub use diesel_helpers::db;
 
 #[macro_use]
 extern crate serde_derive;
 
 fn nas_path<'l>(subdir: Option<&'l str>) -> DonResult<PathBuf> {
-    dotenv()?;
     let mut nas_path = PathBuf::from(var("NAS_PATH")?);
     if let Some(subdir) = subdir {
         nas_path.push(subdir);
