9665aea971d9c093940d4be319cf6ef7832fcbce
RomainC <bebert64@gmail.com>
2023-11-07 08:37:34 +0100

    remove used once enum

--- src/diesel_helpers.rs
+++ src/diesel_helpers.rs
@@ -6,24 +6,14 @@ use {
 };
 
 pub fn db() -> DonResult<PgConnection> {
-    let mode = match env::var("MODE").ok().as_deref() {
-        Some("PROD") => Mode::Prod,
-        _ => Mode::Dev,
-    };
     let user = env::var("RW_USERNAME")?;
     let password = env::var("RW_PASSWD")?;
     let ip = env::var("DB_IPV6")?;
     let port = env::var("DB_PORT")?;
-    let db_name = match mode {
-        Mode::Dev => env::var("DB_NAME_DEV")?,
-        Mode::Prod => env::var("DB_NAME_PROD")?,
+    let db_name = match env::var("MODE").ok().as_deref() {
+        Some("PROD") => env::var("DB_NAME_PROD")?,
+        _ => env::var("DB_NAME_DEV")?,
     };
-    let db_url = format!("postgres://{user}:{password}@[{ip}]:{port}/{db_name}",);
+    let db_url = format!("postgres://{user}:{password}@[{ip}]:{port}/{db_name}");
     Ok(PgConnection::establish(&db_url)?)
-}
-
-#[derive(Debug)]
-enum Mode {
-    Dev,
-    Prod,
 }
