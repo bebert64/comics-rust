8fbbd03ad03ab50e2424efdf3610d95aeae12c30
RomainC <bebert64@gmail.com>
2023-11-28 18:08:35 +0100

    drop some constraints

--- migrations/2023-11-26-113332_unique_path/up.sql
+++ migrations/2023-11-26-113332_unique_path/up.sql
@@ -1,6 +1,7 @@
 ALTER TABLE books
 	ADD CONSTRAINT unique_path UNIQUE (path),
-	DROP CONSTRAINT books_name_check;
+	DROP CONSTRAINT books_name_check,
+	DROP CONSTRAINT books_name_key;
 
 ALTER TABLE books
 	ADD CONSTRAINT books_name_check CHECK (length(title) >= 1);
